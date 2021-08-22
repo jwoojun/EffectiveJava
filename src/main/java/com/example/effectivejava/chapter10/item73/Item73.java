cbd346cdbff809b4692d6204366e853cdd5d74f0
jwoojun <jjwjun10@gmail.com>
2021-08-22 22:45:01 +0900

    exception

---
+++ src/main/java/com/example/effectivejava/chapter10/item73/Item73.java
@@ -0,0 +1,9 @@
+package com.example.effectivejava.chapter10.item73;
+
+import java.util.AbstractSequentialList;
+
+public class Item73 {
+    public static void main(String args []){
+        AbstractSequentialList
+    }
+}
